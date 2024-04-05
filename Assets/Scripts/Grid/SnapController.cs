24e17621cf17990759eb436294e1d453b9a81fcb
CubeOf10 <cubeof10@gmail.com>
2024-04-05 22:41:56 +1000

    Snappable to Dots

---
+++ Assets/Scripts/Grid/SnapController.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SnapController : MonoBehaviour
+{
+    public List<Transform> snapPoints;
+    public List<Draggable> draggableObjects;
+    public float snapRange = 0.5f;
+    void Start()
+    {
+        foreach(Draggable draggable in draggableObjects)
+        {
+            draggable.dragEndedCallback = OnDragEnded;
+        }
+    }
+
+    // Update is called once per frame
+    private void OnDragEnded(Draggable draggable)
+    {
+        float closestDistance = -1;
+        Transform closestSnapPoint = null;
+
+        foreach(Transform snapPoint in snapPoints)
+        {
+            float currentDistance = Vector2.Distance(draggable.transform.localPosition, snapPoint.localPosition);
+            Debug.Log(currentDistance + " " + snapPoint.transform.name);
+
+            if(closestSnapPoint == null || currentDistance < closestDistance)
+            {
+                Debug.Log("New Closest: " + snapPoint.name);
+                closestSnapPoint= snapPoint;
+                closestDistance = currentDistance;
+            }
+        }
+
+        if(closestSnapPoint != null && closestDistance <= snapRange)
+        {
+            Debug.Log("found it: " + closestSnapPoint.name);
+            draggable.transform.localPosition = closestSnapPoint.localPosition;
+        }
+    }
+}
--- Assets/Scripts/Units/SpawnUnit.cs
+++ Assets/Scripts/Units/SpawnUnit.cs
@@ -1,23 +1,32 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 public class SpawnUnit : MonoBehaviour
 {
-    public GameObject unitToSpawn;
-    public Vector3 spawnPosition;
+    public List<GameObject> UnitList;
     GameObject unitHolder;
     void Start()
     {
         unitHolder = GameObject.Find("UnitHolder");
     }
 
-    // Update is called once per frame
-    void Update()
+    public void CreateUnit(string chosenName, int x, int y, int z)//, Vector3 spawnPos)
     {
-        if(Input.GetKeyDown(KeyCode.Backspace))
+        foreach(GameObject troop in UnitList)
         {
-            GameObject newUnit = Instantiate(unitToSpawn, unitHolder.transform);
-            newUnit.transform.position = spawnPosition; 
+           
+            if(troop.name == chosenName)
+            {
+                GameObject newTroop = Instantiate(troop, unitHolder.transform);
+                newTroop.transform.position = new Vector3(x, y, z);
+            }
         }
     }
+
+    public void TestButtons(int yes, int no)
+    {
+        Debug.Log(yes + no);
+    }
 }
+
